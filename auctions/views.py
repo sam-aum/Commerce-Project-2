cc7be98c3abae84e7029f66cd774d9b456cae2e8
Sam Aum <sam_aum@yahoo.com>
2024-04-03 21:31:30 -0500

    added category button in create form

--- auctions/views.py
+++ auctions/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category, Listing
 
 
 def index(request):
@@ -13,7 +13,10 @@ def index(request):
 # Create Listings
 def createListings(request):
     if request.method == "GET":
-        return render(request, "auctions/create.html")
+        allCategories = Category.objects.all()
+        return render(request, "auctions/create.html", {
+            "categories": allCategories
+        })
 
 # User
 def login_view(request):
